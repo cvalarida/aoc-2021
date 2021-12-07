3f71e8dbc05786510c7bb15dc0fb1de24c9cfac0
Christopher Valarida <cvalarida@gmail.com>
2021-12-06 22:12:57 -0800

    Scaffold day 4

---
+++ day_04/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def puzzle_1(numbers, tables):
+    pass
+
+
+def puzzle_2(numbers, tables):
+    pass
+
+
+def parse_input(puzzle_input):
+    pass
+
+
+def run_puzzle(p, puzzle_input):
+    numbers, tables = parse_input(puzzle_input)
+    if p == 1:
+        puzzle_1(numbers, tables)
+    else:
+        puzzle_2(numbers, tables)
