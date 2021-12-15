fa9e9045babfc458c4f8a4975dd4a900d56de831
Christopher Valarida <cvalarida@gmail.com>
2021-12-14 22:55:10 -0800

    Day 14, puzzle 1

--- day_14/main.py
+++ day_14/main.py
@@ -12,52 +12,10 @@ def puzzle_1(template, rules):
     total_elements = Counter()
     print(f"Total length: {len(template)}")
     for i in range(len(template) - 1):
-        print(f"i: {i}")
-        first_polymer = grow_polymer(template[i : i + 2], rules, 10)
-        total_elements = total_elements + count_elements(first_polymer)
-
-        for j in range(len(first_polymer) - 1):
-            second_polymer = grow_polymer(first_polymer[j : j + 2], rules, 10)
-            total_elements = total_elements + count_elements(second_polymer)
-
-            for k in range(len(second_polymer) - 1):
-                third_polymer = grow_polymer(second_polymer[k : k + 2], rules, 10)
-                total_elements = total_elements + count_elements(third_polymer)
-
-                for l in range(len(third_polymer) - 1):
-                    fourth_polymer = grow_polymer(third_polymer[l : l + 2], rules, 10)
-                    total_elements = total_elements + count_elements(fourth_polymer)
-
-                    for m in range(len(fourth_polymer) - 1):
-                        fifth_polymer = grow_polymer(
-                            fourth_polymer[m : m + 2], rules, 10
-                        )
-                        total_elements = total_elements + count_elements(fifth_polymer)
-
-                        for n in range(len(fifth_polymer) - 1):
-                            print(f"  n: {n}")
-                            sixth_polymer = grow_polymer(
-                                fifth_polymer[n : n + 2], rules, 10
-                            )
-                            total_elements = total_elements + count_elements(
-                                sixth_polymer
-                            )
-
-                            for o in range(len(sixth_polymer) - 1):
-                                seventh_polymer = grow_polymer(
-                                    sixth_polymer[o : o + 2], rules, 10
-                                )
-                                total_elements = total_elements + count_elements(
-                                    seventh_polymer
-                                )
-
-                                for p in range(len(seventh_polymer) - 1):
-                                    eighth_polymer = grow_polymer(
-                                        seventh_polymer[p : p + 2], rules, 10
-                                    )
-                                    total_elements = total_elements + count_elements(
-                                        eighth_polymer
-                                    )
+        polymer = grow_polymer(template[i : i + 2], rules, 10)
+        print(f"i: {i} ({template[i: i + 2]} -> {len(polymer)})")
+        total_elements = total_elements + count_elements(polymer)
+    total_elements[template[0]] += 1  # Don't forget to count the first element
 
     sorted = total_elements.most_common()
     return sorted[0][1] - sorted[-1][1]
@@ -81,11 +39,11 @@ def memoize_count(func):
 @memoize_count
 def count_elements(polymer):
     # Iterate over the string and increment each element count
-    elements = {}
-    for c in polymer:
-        if c not in elements:
-            elements[c] = 0
-        elements[c] += 1
+    elements = {char: 0 for char in polymer}
+    # Ignore the first character so we don't double-count.
+    # Yes, I know it's hacky. -.-
+    for i in range(1, len(polymer)):
+        elements[polymer[i]] += 1
 
     return Counter(elements)
 
